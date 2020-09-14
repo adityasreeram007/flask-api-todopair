0e041f59b18335e322c6bce17cb5a8d89bc524c0
aditya <adityasreeram99@gmail.com>
2020-09-14 14:18:40 -0500

    make it better

---
+++ wsgi.py
@@ -0,0 +1,23 @@
+from flask  import Flask,jsonify
+import pandas
+data=pandas.read_csv('auth.csv')
+uname=list(data['Username'])
+upass=list(data['Password'])
+uteams=list(data['Teamname'])
+
+app=Flask(__name__)
+@app.route('/<par>',methods=['GET'])
+def sender(par):
+    ret={'status':'False','Team':'False'}
+    cred=par.split('&')
+    print(cred[0].split('=')[1])
+    if cred[0].split('=')[1] in uname:
+        ind=uname.index(cred[0].split('=')[1])
+        if upass[ind]==cred[1].split('=')[1]:
+            ret['status']='True'
+            ret['Team']=uteams[ind]
+    return jsonify(ret)
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
